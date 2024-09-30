0aabc224d9a99c518b64d2e3700399eeba6ee2bd
IvanSeagull <ivan.seagull.k@gmail.com>
2024-09-30 11:35:21 +0100

    update gameController script: lines + dinamic getting pickups

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,18 +5,48 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     private LineRenderer lineRenderer;
-    public GameObject[] pickups;
-    // Start is called before the first frame update
-    void Start()
-    {
-        lineRenderer = gameObject.AddComponent<LineRenderer>();
-    }
+    public Transform player;
+    public float detectionRadius = 20f; 
 
-    // Update is called once per frame
-    void Update()
-    {
+
+    void Start(){
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+        GameObject[] pickups = GameObject.FindGameObjectsWithTag("PickUp");
+
         for(int i=0; i<pickups.Length; i++) {
             pickups[i].GetComponent<Renderer>().material.color = Color.blue;
         }
+
+    }
+
+    void FixedUpdate(){
+
+        GameObject closestPickup = FindClosestActivePickup();
+
+
+        if (closestPickup != null){
+            lineRenderer.SetPosition(0, player.position);
+            lineRenderer.SetPosition(1, closestPickup.transform.position);
+        }
+        else{
+            lineRenderer.enabled = false;
+        }
+    }
+
+    GameObject FindClosestActivePickup(){
+        GameObject[] pickups = GameObject.FindGameObjectsWithTag("PickUp");
+        GameObject closest = null;
+        float minDistance = Mathf.Infinity;
+
+        foreach (GameObject pickup in pickups){
+            if (!pickup.activeInHierarchy) continue;
+            float distance = Vector3.Distance(player.position, pickup.transform.position);
+            if (distance < minDistance && distance <= detectionRadius){
+                closest = pickup;
+                minDistance = distance;
+            }
+        }
+
+        return closest;
     }
 }
