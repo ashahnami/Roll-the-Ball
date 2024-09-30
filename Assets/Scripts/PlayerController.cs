b970fe10c9f4a2b6e3fef3d71c94b5cac6ad0375
Armin Shahnami <shahnami.armin@gmail.com>
2024-09-30 10:52:05 +0100

    Fix error

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.InputSystem;using UnityEngine.UI;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
